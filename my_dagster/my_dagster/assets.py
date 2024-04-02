a40aed611f6bde45d2ac85d5c191fa9622ce0a48
Gabriel Crestani <gabrielpcrestani@gmail.com>
2024-04-02 08:36:33 -0400

    ETL process working on dagster

--- my_dagster/my_dagster/assets.py
+++ my_dagster/my_dagster/assets.py
@@ -4,6 +4,10 @@ from dagster import (
     DailyPartitionsDefinition,
     MaterializeResult,
 )
+import pandas as pd
+
+from . import utils
+from .resources import SourceDBResource, TargetDBResource
 
 
 @asset
@@ -11,9 +15,27 @@ def hello_world() -> MaterializeResult:
     return MaterializeResult(metadata={"message": "Hello world"})
 
 
-@asset(partitions_def=DailyPartitionsDefinition(start_date="2024-01-31"))
-def my_daily_partitioned_asset(context: AssetExecutionContext) -> None:
-    partition_date_str = context.partition_key
+# @asset(partitions_def=DailyPartitionsDefinition(start_date="2024-01-31"))
+@asset()
+def my_daily_partitioned_asset(
+    context: AssetExecutionContext,
+    source_db: SourceDBResource,
+    target_db: TargetDBResource,
+) -> None:
+    # partition_date_str = context.partition_key
+    # url = f"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={partition_date_str}"
+    # target_location = f"nasa/{partition_date_str}.csv"
 
-    url = f"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={partition_date_str}"
-    target_location = f"nasa/{partition_date_str}.csv"
+    VARS = ["wind_speed", "power"]
+    AGG_FUNCS = ["mean", "max", "min", "std"]
+
+    # Extract
+    df_source = source_db.get_data(input_date="2024-01-31", fields=VARS)
+    with pd.option_context("display.max_rows", None, "display.max_columns", None):
+        print(df_source)
+
+    # Transform
+    df_agg = utils.aggregate_data(df_source, agg_funcs=AGG_FUNCS)
+
+    # Load
+    target_db.post_data(df_agg)
--- my_dagster/my_dagster/resources/__init__.py
+++ my_dagster/my_dagster/resources/__init__.py
@@ -1,12 +1,86 @@
+import os
+
 from dagster import ConfigurableResource
 import httpx
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 class SourceDBResource(ConfigurableResource):
-    def get_data(self, endpoint: str) -> httpx.Response:
-        return
+    def get_data(self, input_date: str, fields: list[str]) -> pd.DataFrame:
+        SOURCE_DB_API_HOST_PORT = 8081
+        base_url = f"http://localhost:{SOURCE_DB_API_HOST_PORT}"
+        body = {
+            "start_datetime": f"{input_date}T07:00:00",
+            "end_datetime": f"{input_date}T08:59:59",
+            "fields": fields,
+        }
+        try:
+            res = httpx.post(f"{base_url}/data", json=body)
+            res.raise_for_status()
+
+            res_json = res.json()
+            # logging.debug(res_json)
+            df = pd.DataFrame(res_json)
+
+            if df.empty:
+                raise SystemExit("Exiting program: Empty data source.")
+
+            return df
+        except httpx.HTTPError as exc:
+            # logging.error(f"HTTP Exception for {exc.request.url} - {exc}")
+            raise SystemExit("Exiting program: Source data fetch error.")
 
 
 class TargetDBResource(ConfigurableResource):
-    def post_data(self, endpoint: str) -> httpx.Response:
-        return
+    def post_data(self, df_agg: pd.DataFrame) -> None:
+        DB_USER = os.getenv("POSTGRES_USER")
+        DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+        DB_DATABASE_NAME = os.getenv("POSTGRES_DB")
+
+        DB_HOST_PORT = os.getenv("DB_HOST_PORT")
+        DB_HOST_NAME = "localhost"
+        # if is_inside_docker:
+        #     DB_HOST_NAME = DB_DATABASE_NAME
+        # else:
+        #     DB_HOST_NAME = "localhost"
+
+        # Database connection string
+        SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST_NAME}:{DB_HOST_PORT}/{DB_DATABASE_NAME}"
+        # logging.debug(f"SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}")
+
+        engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+        #####################
+        #####################
+
+        df_signals = pd.read_sql_table("signal", con=engine)
+        # logging.debug(df_signals)
+
+        df_melted = pd.melt(
+            df_agg,
+            id_vars=["timestamp", "func"],
+            value_vars=["wind_speed", "power"],
+            var_name="name",
+        )
+        with pd.option_context("display.max_rows", None, "display.max_columns", None):
+            # logging.debug(df_melted)
+            df_melted_merged = df_melted.merge(
+                df_signals,
+                how="left",
+                on=["name", "func"],
+            )
+            df_melted_merged.rename(columns={"id": "signal_id"}, inplace=True)
+            # logging.debug(df_melted_merged)
+
+            df_melted_merged.drop(["name", "func"], axis=1, inplace=True)
+            # logging.debug(df_melted_merged)
+
+            # Sort data
+            df_melted_merged.sort_values(by=["timestamp", "signal_id"], inplace=True)
+
+        rows_affected = df_melted_merged.to_sql(
+            "data", con=engine, if_exists="append", index=False
+        )
+        print(f"Rows affected: {rows_affected}")
+        # logging.debug(f"Rows affected: {rows_affected}")

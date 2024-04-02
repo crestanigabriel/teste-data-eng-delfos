278acbdac5876d83aa2ebe20a8d59b9398d77c50
Gabriel Crestani <gabrielpcrestani@gmail.com>
2024-04-02 15:09:37 -0400

    Adjusting mock data generator script and generating correct data

--- source_db/create_mock_data.py
+++ source_db/create_mock_data.py
@@ -2,13 +2,16 @@
 
 import csv
 import datetime as dt
+import os
 import random
 
 
 def generate_csv(initial_date: dt.datetime, freq: int, interval: int) -> None:
     final_date = initial_date + dt.timedelta(days=interval)
 
-    with open("dataset/data.csv", "w") as f:
+    with open(
+        os.path.dirname(os.path.abspath(__file__)) + "/dataset/data.csv", "w"
+    ) as f:
         writer = csv.writer(f)
         writer.writerow(["timestamp", "wind_speed", "power", "ambient_temperature"])
 
@@ -28,8 +31,9 @@ def generate_csv(initial_date: dt.datetime, freq: int, interval: int) -> None:
 
 if __name__ == "__main__":
     random.seed(12345)
-    initial_date = dt.datetime(year=2024, month=1, day=31, hour=7, minute=22, second=37)
-    TIMESTAMP_FREQ = 10  # in minutes
-    TOTAL_TIMESTAMP_INTERVAL = 2  # in days
+    initial_date = dt.datetime(year=2024, month=1, day=1, hour=1, minute=30, second=0)
+    TIMESTAMP_FREQ = 1  # in minutes
+    TOTAL_TIMESTAMP_INTERVAL = 10  # in days
 
     generate_csv(initial_date, TIMESTAMP_FREQ, TOTAL_TIMESTAMP_INTERVAL)
+    print("source_db/dataset/data.csv generated!")
